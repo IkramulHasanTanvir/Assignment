3b3879b99b693f9b17bc3bc95ce41715fc517a1a
ikramulhasantanvir <ikramulhasantanvir@gmail.com>
2024-06-16 08:43:10 +0600

    car_shop

--- assignment_1/car_shop.dart
+++ assignment_1/car_shop.dart
@@ -1,26 +1,26 @@
 import 'car.dart';
 
 main() {
-  Car car = Car(
+  Car tanvirIs = Car(
     brand: 'Toyota',
     model: 'CHR',
     year: 2016,
   );
-  car.drive(2000.0);
+  tanvirIs.drive(2000.0);
 
-  Car car1 = Car(
+  Car hridoyIs = Car(
     brand: 'Mazda',
     model: 'MX-5',
     year: 2014,
   );
-  car1.drive(900.0);
+  hridoyIs.drive(900.0);
 
-  Car car2 = Car(
+  Car rafatIs = Car(
     brand: 'Honda',
     model: 'Civic',
     year: 2017,
   );
-  car2.drive(500.0);
+  rafatIs.drive(500.0);
 
   print('Total number of cars created: ${Car.numberOfCars}');
 }
